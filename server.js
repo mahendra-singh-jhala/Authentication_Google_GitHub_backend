0c96bd3e0c411b4aaf379e6063150f883b96ab5a
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2025-01-22 17:18:48 +0530

    first commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectdb();
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000',  
+    origin: 'https://google-github-authentication.netlify.app',  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true 
 }));
